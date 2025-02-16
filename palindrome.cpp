15f9ffb0c5abfc43561f2fe328621fbf5a36b634
Karthiswaran-R <karthi28974@gmail.com>
2025-02-16 23:10:14 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,rev=0,rem, org;
+    cout << "Enter a number: ";
+    cin >> num;
+    org=num;
+    while (num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num/=10;
+    }
+    if (org == rev){
+        cout <<org<<" is a palindrome." <<endl;
+    }else{
+        cout <<org<<" is not a palindrome."<<endl;
+    }
+    return 0;
+}
